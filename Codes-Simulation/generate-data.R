7ed82de0fbe6fa652aa2c52cc7e8b149b24b64ca
Franziaupperle <51368590+Franziaupperle@users.noreply.github.com>
2021-05-09 18:06:33 +0200

    Update generate-data.R

--- Codes-Simulation/generate-data.R
+++ Codes-Simulation/generate-data.R
@@ -1,7 +1,12 @@
 
+# FUNCTION TO GENERATE ALL RELEVANT DATA 
 
+# pf: selected parameters
+# Rd2: coefficient of determination in the treatment equation
+# Ry2: coefficient of determination in the main equation
 generate.data = function(pf, Rd2, Ry2) {
   
+  # get parameter
   Sigma = pf$Sigma
   gamma = pf$gamma
   beta = pf$beta
@@ -12,11 +17,14 @@ generate.data = function(pf, Rd2, Ry2) {
   p = pf$p
   n = pf$n
   
+  # generate covariates and error terms
   x = mvrnorm(n = n,
               mu = rep(0, p),
               Sigma = Sigma)
   nu = rnorm(n, 0, 1)
   w = rnorm(n, 0, 1)
+  
+  # calculate disturbance terms to get desired Rd2 and Ry2
   
   vec.d = seq(0.1, 1.6, by = 0.1)
   Rd.2 = NULL
@@ -29,6 +37,7 @@ generate.data = function(pf, Rd2, Ry2) {
   }
   
   cd = vec.d[which.min(abs(Rd.2 - Rd2))]
+  # calculate the final treatment d
   d = 0.5 + x %*% gamma * cd + nu
   
   for (k in 1:length(vec.y)) {
@@ -37,12 +46,13 @@ generate.data = function(pf, Rd2, Ry2) {
   }
   
   cy =  vec.y[which.min(abs(Ry.2 - Ry2))]
-
+  # calculate the final dependend variable
   y = 1 + alpha0 * d + x %*% beta * cy + w
   
   print(cd)
   print(cy)
   
+  # return the generated data 
   result = list(x = x,
                 d = d,
                 y = y)
