817ccc2ac74c8ac18cbfaedf920e3135d3199999
Franziaupperle <51368590+Franziaupperle@users.noreply.github.com>
2021-05-09 16:41:52 +0200

    Update Double.R

--- Codes-Simulation/Double.R
+++ Codes-Simulation/Double.R
@@ -20,6 +20,7 @@ double.mcfun = function(no_runs_mc) {
     Ry2 = Ry2
   )
   
+  # function to execute simulation iterations parallel
   
   double.parallel = function(no_runs_mc, pf, Rd2, Ry2) {
     
