48e60166a8d949ae151d3bde7d47201a953e46b5
Franziaupperle <51368590+Franziaupperle@users.noreply.github.com>
2021-05-09 20:42:24 +0200

    Update Functions.R

--- Codes-RentData/Functions.R
+++ Codes-RentData/Functions.R
@@ -1,23 +1,31 @@
 
 # R-SPLIT
 
+# y: dependent variable
+# d: treatment variable
+# x: covariates
+# B: number of subsampling iterations
 Split.smooth <- function(y, d, x, B) {
   
   z = cbind(d,x)
   n = length(y)
   p = length(x[1,])
   
+  # declare variables for the results
   alpha.est = NULL
   alpha.se = NULL
   M.all = c()
   model.size = c()
   
+  # declare matrix used to store the selection indices of the sample
   Y.count = matrix(data = NA, nrow = B, ncol = n)
+  # declare matrix used to store the selection indices of the control variables
   C.count = matrix(data = NA, nrow = B, ncol = p)
   
   for (b in 1:B) {
     
     set.seed(b+12345)
+    # start repeated subsampling, model selection and treatment estimation
     index.subsam = sample(seq(1,n), 0.7*n) 
     y.s = y[index.subsam]
     z.s = z[index.subsam,]
@@ -29,8 +37,9 @@ Split.smooth <- function(y, d, x, B) {
     d.t = z.t[,1]
     Y.count[b,] = tabulate(index.subsam,n)
     
+    # select model with LASSO Cross-Validation or Rigorous LASSO, which one is chosen is selected in function select.model()
     M.hat = select.model(y = y.s, x = x.s)
-    
+    # refit the model
     alpha.est[b] = lm(y.t ~ d.t + x.t[,M.hat])$coef[2]
     
     model.size[b] = length(M.hat)
@@ -43,6 +52,7 @@ Split.smooth <- function(y, d, x, B) {
     
   }
   
+    # get relative inclusion frequencies of each variable over all subsampling steps
     C.rel = colMeans(C.count)
 
     M.hat09 = which(C.rel > 0.9)
@@ -52,9 +62,11 @@ Split.smooth <- function(y, d, x, B) {
   
     M.Freq = as.data.frame(table(M.all)/B)
 
+  # get the smoothed estimator and its standard error
   alpha.smoothed = mean(alpha.est)
   alpha.se = IF.varestbiascorr(Y.count, alpha.est, n, 0.7*n)
   
+  # return results
   result = list(alpha.smoothed = alpha.smoothed, 
                 alpha.est = alpha.est,
                 alpha.se = alpha.se, 
@@ -74,14 +86,22 @@ Split.smooth <- function(y, d, x, B) {
 
 # PODS-SPLIT
 
+# y: dependent variable
+# d: treatment variable
+# x: covariates
+# B: number of subsampling iterations
 PODS.Split <- function(y, d, x, B) {
   
   n = length(y)
   p = length(x[1,])
   
+  # declare matrix used to store the selection indices of the sample
   Y.count = matrix(data = NA, nrow = B, ncol = n)
+  # declare matrix used to store the selection indices of the control variables
   C.count = matrix(data = NA, nrow = B, ncol = p)
   z = cbind(d,x)
+  
+  # declare variables for the results
   model.size = c()
   alpha.est = c()
   M.all = c()
@@ -89,6 +109,7 @@ PODS.Split <- function(y, d, x, B) {
   for(b in 1:B) {
     
     set.seed(b+12345)
+    # start repeated subsampling, model selection and treatment estimation
     index.subsam = sample(seq(1,n), 0.7*n)
     y.s = y[index.subsam]
     z.s = z[index.subsam,]
@@ -101,8 +122,9 @@ PODS.Split <- function(y, d, x, B) {
     x.t = z.t[,-1]
     Y.count[b,] = tabulate(index.subsam,n)
     
+    # select the model
     M.hat = Projection.set(y = y.s, d = d.s, x = x.s)
-    
+    # refit the model
     alpha.est[b] = lm(y.t ~ d.t + x.t[,M.hat])$coefficients[2]
     model.size[b] = length(M.hat)
     
@@ -114,6 +136,7 @@ PODS.Split <- function(y, d, x, B) {
     
   }
 
+  # get relative inclusion frequencies of each variable over all subsampling steps
   C.rel = colMeans(C.count)
   
   M.hat09 = which(C.rel > 0.9)
@@ -121,9 +144,11 @@ PODS.Split <- function(y, d, x, B) {
   M.hat07 = which(C.rel > 0.7)
   M.hat06 = which(C.rel > 0.6)
   
+   # get the smoothed estimator and its standard error
   alpha.smoothed = mean(alpha.est)
   alpha.se = IF.varestbiascorr(Y.count, alpha.est, n, 0.7*n)
   
+  # return results
   result = list(alpha.smoothed = alpha.smoothed, 
                 alpha.est = alpha.est,
                 alpha.se = alpha.se, 
@@ -141,6 +166,10 @@ PODS.Split <- function(y, d, x, B) {
 
 # DOUBLE STABILITY
 
+# y: dependent variable
+# d: treatment variable
+# x: covariates
+# B: number of subsampling iterations
 Double.Stability <- function(y, d, x, B) {
   
   alpha.est = NULL
@@ -155,19 +184,22 @@ Double.Stability <- function(y, d, x, B) {
   M.yx.all = c()
   M.dx.all = c()
   z = cbind(d,x)
+  # declare matrix used to store the selection indices of the control variables associated with the rent price
   Cy.count = matrix(data = NA, nrow = B, ncol = p)
+  # declare matrix used to store the selection indices of the control variables associated with the rental brake
   Cd.count = matrix(data = NA, nrow = B, ncol = p)
   
   for(b in 1:B) {
     
     set.seed(b+12345)
+    # start repeated subsampling, model selection and treatment estimation
     index.subsam = sample(seq(1,n), 0.7*n)
     y.s = y[index.subsam]
     z.s = z[index.subsam,]
     d.s = z.s[,1] 
     x.s = z.s[,-1]
     
-    # DOUBLE SELECTION
+    # DOUBLE SELECTION with LASSO Cross-Validation or Rigorous LASSO, which one is chosen is selected in function select.model()
     M.dx = select.model(y = d.s, x = x.s)
     M.yx = select.model(y = y.s, x = x.s)
     
@@ -180,54 +212,63 @@ Double.Stability <- function(y, d, x, B) {
     
   }
   
+  # get relative inclusion frequencies of the variable selection from regression on the rent price 
   Cy.rel = colMeans(Cy.count)
+  # get relative inclusion frequencies of the variable selection from regression on the rental brake
   Cd.rel = colMeans(Cd.count)
 
+  # get indices of variables with a reltive inclusion frequency higher 0.95
   Cy095 = which(Cy.rel > 0.95)
   Cd095 = which(Cd.rel > 0.95)
   M.hat095 = unique(sort(c(Cy095,Cd095)))
+  # get indices of variables with a reltive inclusion frequency higher 0.9
   Cy09 = which(Cy.rel > 0.9)
   Cd09 = which(Cd.rel > 0.9)
   M.hat09 = unique(sort(c(Cy09,Cd09)))
+  # get indices of variables with a reltive inclusion frequency higher 0.8
   Cy08 = which(Cy.rel > 0.8)
   Cd08 = which(Cd.rel > 0.8)
   M.hat08 = unique(sort(c(Cy08,Cd08)))
+  # get indices of variables with a reltive inclusion frequency higher 0.7
   Cy07 = which(Cy.rel > 0.7)
   Cd07 = which(Cd.rel > 0.7)
   M.hat07 = unique(sort(c(Cy07,Cd07)))
+  # get indices of variables with a reltive inclusion frequency higher 0.6
   Cy06 = which(Cy.rel > 0.6)
   Cd06 = which(Cd.rel > 0.6)
   M.hat06 = unique(sort(c(Cy06,Cd06)))
   
-  
+  # refit the model with indices of variables with a reltive inclusion frequency higher 0.6
   lm06 = lm(y ~ d + x[,M.hat06]) # Post-lasso OLS step
   alpha.est06 = lm06$coef[2]
   lm_HC106 = coeftest(lm06, vcov = vcovHC(lm06, type="HC1"))
   alpha.se06 = lm_HC106[2,2]
 
-
+  # refit the model with indices of variables with a reltive inclusion frequency higher 0.7
   lm07 = lm(y ~ d + x[,M.hat07]) # Post-lasso OLS step
   alpha.est07 = lm07$coef[2]
   lm_HC107 = coeftest(lm07, vcov = vcovHC(lm07, type="HC1"))
   alpha.se07 = lm_HC107[2,2]
 
-
+  # refit the model with indices of variables with a reltive inclusion frequency higher 0.8
   lm08 = lm(y ~ d + x[,M.hat08]) # Post-lasso OLS step
   alpha.est08 = lm08$coef[2]
   lm_HC108 = coeftest(lm08, vcov = vcovHC(lm08, type="HC1"))
   alpha.se08 = lm_HC108[2,2]
 
-  
+  # refit the model with indices of variables with a reltive inclusion frequency higher 0.9
   lm09 = lm(y ~ d + x[,M.hat09]) # Post-lasso OLS step
   alpha.est09 = lm09$coef[2]
   lm_HC109 = coeftest(lm09, vcov = vcovHC(lm09, type="HC1"))
   alpha.se09 = lm_HC109[2,2]
   
+  # REFIT THE MODEL WITH indices of variables with a reltive inclusion frequency higher 0.95
   lm095 = lm(y ~ d + x[,M.hat095]) # Post-lasso OLS step
   alpha.est095 = lm095$coef[2]
   lm_HC1095 = coeftest(lm095, vcov = vcovHC(lm095, type="HC1"))
   alpha.se095 = lm_HC1095[2,2]
   
+  # return results
   result = list(lm06 = summary(lm06),
                 lmhc1_06 = lm_HC106,
                 alpha.est06 = alpha.est06,
@@ -272,8 +313,10 @@ select.model = function(y, x){
   p = length(x[1,])
   M = seq(1,p,1)
   
-  # Perform model selection with either Rigorous Lasso oder Cross-Validation
+  # Perform model selection with either Rigorous Lasso (line 320 - 326) or Cross-Validation (line 329 - 342)
+  # one LASSO method always has to be commented out 
 
+  
   # index.lasso = rlasso(y ~ x)$index
   # 
   # M.hat = sort(M[index.lasso], decreasing = FALSE)
@@ -302,6 +345,7 @@ select.model = function(y, x){
 }
 
 # Function to calculate standard error in R-Split and PODS-Split 
+# used as in the simualtion studies 
 
 IF.varestbiascorr = function(Y.count, alpha.est, n, split.size) {
   
@@ -320,6 +364,7 @@ IF.varestbiascorr = function(Y.count, alpha.est, n, split.size) {
 }
 
 # Function to select a model and perform OLS in PODS
+# used as in the simulation studies
 
 Projection = function(y, d, x) { 
   
@@ -327,30 +372,30 @@ Projection = function(y, d, x) {
   n = length(y)
   
   M = seq(1,p)
-  fit.dx = rlasso(d ~ x)$index # Step 1: select a set of variables M_hat_D which are associated with D
+  fit.dx = rlasso(d ~ x)$index 
   
-  if(sum(fit.dx) == 0){ # index of selected variables (logical vector)
+  if(sum(fit.dx) == 0){ 
     fit.dx = 1
   }
   
-  M.dx = M[fit.dx] # FRISCH WAUGH THEOREM - variables, that are associated with D are kicked out
+  M.dx = M[fit.dx] 
   
-  x.res = lm(x[,-M.dx]~ d + x[,M.dx])$res # Step 2: to remove the components associated with D, we project (X, Y) onto a space which is orthogonal
-  y.res = lm(y ~ d + x[,M.dx])$res # to the space spanned by D and X_M_hat_D
+  x.res = lm(x[,-M.dx]~ d + x[,M.dx])$res 
+  y.res = lm(y ~ d + x[,M.dx])$res 
   
-  fit.yx = rlasso(y.res ~ x.res)$index  # Step 3: selecting additional variables that are expected to have low correlation with D and so the
+  fit.yx = rlasso(y.res ~ x.res)$index  
   
-  temp0 = M[-M.dx]; # over-fitting bias can be controlled; select a model fit.yx for regressing y.res on x.res
-  if(sum(fit.yx) == 0){ # select a model M_hat_Y_star for regressing Y on D and X_M_hat_star
+  temp0 = M[-M.dx]; 
+  if(sum(fit.yx) == 0){ 
     fit.yx = 1
   }
   
   M.yx = temp0[fit.yx] 
   
-  M.hat = sort(c(M.dx, M.yx)) # unite relevant covariates 
+  M.hat = sort(c(M.dx, M.yx))
   
   reg.dx = lm(d ~ x[,M.hat])
-  reg.yx = lm(y ~ d + x[,M.hat]) # Step 4: Regress Y on D and M.hat to get alpha.hat, which is the estimated coefficient of D
+  reg.yx = lm(y ~ d + x[,M.hat]) 
   
   nu = reg.dx$residuals
   eps = reg.yx$residuals*sqrt(n/(n - length(M.hat) - 1))
