3ac3a2b352fdbf8c3c0c013b9e5200e384f24c57
Franziaupperle <51368590+Franziaupperle@users.noreply.github.com>
2021-05-09 16:38:23 +0200

    Update Double.R

--- Codes-Simulation/Double.R
+++ Codes-Simulation/Double.R
@@ -3,6 +3,7 @@
 
 double.mcfun = function(no_runs_mc) {
   
+  # generate relevant parameters
   pf = generate.parameter(
     n = n,
     p = p,
@@ -18,6 +19,7 @@ double.mcfun = function(no_runs_mc) {
     Ry2 = Ry2
   )
   
+  # start parallel computing
   double.parallel = function(no_runs_mc, pf, Rd2, Ry2) {
     
     alpha.est = NULL
